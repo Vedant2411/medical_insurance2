90f2ea53f3e15307eb04ad6550e12b4b4c65eef1
Vedant Sharma <sharmavedant777@gmail.com>
2025-05-07 02:58:14 +0530

    added all files to main

---
+++ hello.py
@@ -0,0 +1,28 @@
+# def get_addition(a,b):
+#     add = a + b
+#     return add
+# PRINT() # CTRL +S : SAVE
+# print()
+
+# from calculations.division import get_division # 
+# from calculations.multiplication import multiplication,var1
+# # import calculations as nm
+
+# print(" we are in calculation file __name__",__name__) #__main__
+
+# if __name__ == "__main__":
+#     get_division(10,2)
+#     multiplication(2,2)
+
+
+# from calculations import subtract , addition
+# from calculations.subtract import var1
+# print(" we are in hello file __name__",__name__) #__main__
+# if __name__ == "__main__":
+#     print(subtract.subtraction(10,5))
+#     print(addition.addition(10,5))
+#     print(var1)
+
+
+
+
